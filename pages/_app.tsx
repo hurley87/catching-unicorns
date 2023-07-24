d438b04df7e683bd1b87375e23eda4c3526370f9
David Hurley <dhurls99@gmail.com>
2023-07-24 10:01:58 -0400

    remove flow from project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,6 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
-import * as fcl from '@onflow/fcl';
-
-fcl.config({
-  'flow.network': 'mainnet',
-  'app.detail.title': 'Catching Unicorns',
-  'accessNode.api': 'https://rest-mainnet.onflow.org',
-  'app.detail.icon': 'https://www.catchingunicorns.ca/Cover.png',
-  'discovery.wallet': `https://wallet-v2.blocto.app/${process.env.NEXT_PUBLIC_DAPP_ID}/flow/authn`,
-});
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
