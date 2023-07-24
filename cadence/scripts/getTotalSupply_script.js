e0476fd5ca54e85a8af78bb8c9944362b04de19c
David Hurley <dhurls99@gmail.com>
2023-07-24 09:47:23 -0400

    remove all flow related work

--- cadence/scripts/getTotalSupply_script.js
+++
@@ -1,9 +0,0 @@
-export const getTotalSupply =
-  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-  `
-import CatchingUnicorns from 0x117abe70f3e72975;
-
-pub fun main(): UInt64 {
-  return CatchingUnicorns.totalSupply;
-}
-`;
